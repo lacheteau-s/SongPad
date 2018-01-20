49e3b9f21a4effa4b144aa94f616b002b599a3f2
Sullivan Lacheteau <lacheteau.s@gmail.com>
2018-01-20 19:27:09 +0100

    Basic menu with commands

--- SongPad/ViewModels/MainWindowViewModel.cs
+++ SongPad/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SongPad.Tools;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace SongPad.ViewModels
 {
 	public class MainWindowViewModel
 	{
+		public MenuViewModel MenuViewModel => IoC.GetInstance<MenuViewModel>();
 	}
 }
---
+++ SongPad/ViewModels/MenuViewModel.cs
@@ -0,0 +1,38 @@
+﻿using SongPad.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SongPad.ViewModels
+{
+	public class MenuViewModel
+	{
+		public ICommand NewCommand => new Command(OnNew);
+		public ICommand OpenCommand => new Command(OnOpen);
+		public ICommand SaveCommand => new Command(OnSave);
+		public ICommand QuitCommand => new Command(OnQuit);
+
+		public void OnNew()
+		{
+
+		}
+
+		public void OnOpen()
+		{
+
+		}
+
+		public void OnSave()
+		{
+
+		}
+
+		public void OnQuit()
+		{
+
+		}
+	}
+}
