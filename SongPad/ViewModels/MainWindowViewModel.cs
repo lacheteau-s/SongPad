9fe9005e9b5f58fe0ba9664a5fc56fcdc50669fb
Sullivan Lacheteau <lacheteau.s@gmail.com>
2018-01-21 14:04:18 +0100

    Menu all rigged up

--- SongPad/ViewModels/MainWindowViewModel.cs
+++ SongPad/ViewModels/MainWindowViewModel.cs
@@ -9,6 +9,48 @@ namespace SongPad.ViewModels
 {
 	public class MainWindowViewModel
 	{
-		public MenuViewModel MenuViewModel => IoC.GetInstance<MenuViewModel>();
+		public MenuViewModel MenuViewModel { get; private set; }
+
+		public MainWindowViewModel()
+		{
+			MenuViewModel = IoC.GetInstance<MenuViewModel>();
+		}
+
+		public void Initialize()
+		{
+			// TODO: replace with messaging
+			MenuViewModel.NewEventHandler += OnMenuNew;
+			MenuViewModel.OpenEventHandler += OnMenuOpen;
+			MenuViewModel.SaveEventHandler += OnMenuSave;
+			MenuViewModel.QuitEventHandler += OnMenuQuit;
+		}
+
+		public void Shutdown()
+		{
+			MenuViewModel.NewEventHandler -= OnMenuNew;
+			MenuViewModel.OpenEventHandler -= OnMenuOpen;
+			MenuViewModel.SaveEventHandler -= OnMenuSave;
+			MenuViewModel.QuitEventHandler -= OnMenuQuit;
+		}
+
+		private void OnMenuNew(object sender, EventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void OnMenuOpen(object sender, EventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void OnMenuSave(object sender, EventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void OnMenuQuit(object sender, EventArgs e)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
--- SongPad/ViewModels/MenuViewModel.cs
+++ SongPad/ViewModels/MenuViewModel.cs
@@ -15,24 +15,30 @@ namespace SongPad.ViewModels
 		public ICommand SaveCommand => new Command(OnSave);
 		public ICommand QuitCommand => new Command(OnQuit);
 
+		// TODO: replace with MessagingService
+		public event EventHandler NewEventHandler;
+		public event EventHandler OpenEventHandler;
+		public event EventHandler SaveEventHandler;
+		public event EventHandler QuitEventHandler;
+
 		public void OnNew()
 		{
-
+			NewEventHandler?.Invoke(this, EventArgs.Empty);
 		}
 
 		public void OnOpen()
 		{
-
+			OpenEventHandler?.Invoke(this, EventArgs.Empty);
 		}
 
 		public void OnSave()
 		{
-
+			SaveEventHandler?.Invoke(this, EventArgs.Empty);
 		}
 
 		public void OnQuit()
 		{
-
+			QuitEventHandler?.Invoke(this, EventArgs.Empty);
 		}
 	}
 }
