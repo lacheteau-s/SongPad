2eb3238763ffc5f8d47a8810eaedc816f3992cf6
Sullivan Lacheteau <lacheteau.s@gmail.com>
2018-02-14 16:59:13 +0100

    Skin implementation with design time rendering

--- SongPad/App.xaml.cs
+++ SongPad/App.xaml.cs
@@ -16,6 +16,14 @@ namespace SongPad
 		public App()
 		{
 			InitializeComponent();
+			ApplySkin();
+		}
+
+		// https://www.codeproject.com/Articles/19782/Creating-a-Skinned-User-Interface-in-WPF
+		public void ApplySkin()
+		{
+			var skin = LoadComponent(new Uri(@".\Resources\Skins\DarkSkin.xaml", UriKind.Relative)) as ResourceDictionary;
+			Resources.MergedDictionaries.Add(skin);
 		}
     }
 }
