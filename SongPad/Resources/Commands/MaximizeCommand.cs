aa3d0a89ff512fc2089e083db8b9c68246547c35
Sullivan Lacheteau <lacheteau.s@gmail.com>
2018-02-18 18:14:05 +0100

    Maximize/Minimize window

---
+++ SongPad/Resources/Commands/MaximizeCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SongPad.Resources.Commands
+{
+	public class MaximizeCommand : ICommand
+	{
+		public event EventHandler CanExecuteChanged;
+
+		public bool CanExecute(object parameter)
+		{
+			return true;
+		}
+
+		public void Execute(object parameter)
+		{
+			var window = parameter as Window;
+
+			if (window != null)
+				window.WindowState = (window.WindowState == WindowState.Maximized) ? WindowState.Normal : WindowState.Maximized;
+		}
+	}
+}
---
+++ SongPad/Resources/Commands/MinimizeCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SongPad.Resources.Commands
+{
+	public class MinimizeCommand : ICommand
+	{
+		public event EventHandler CanExecuteChanged;
+
+		public bool CanExecute(object parameter)
+		{
+			return true;
+		}
+
+		public void Execute(object parameter)
+		{
+			var window = parameter as Window;
+
+			if (window != null)
+				window.WindowState = WindowState.Minimized;
+		}
+	}
+}
