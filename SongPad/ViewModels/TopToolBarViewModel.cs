19c15781176e8d44da35c84c77976c24b55df9a5
Sullivan Lacheteau <lacheteau.s@gmail.com>
2018-02-28 23:15:36 +0100

    New project from top toolbar

--- SongPad/ViewModels/TopToolBarViewModel.cs
+++ SongPad/ViewModels/TopToolBarViewModel.cs
@@ -1,4 +1,6 @@
-﻿using SongPad.Tools;
+﻿using SongPad.Messages;
+using SongPad.Services;
+using SongPad.Tools;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,15 +12,22 @@ namespace SongPad.ViewModels
 {
 	public class TopToolBarViewModel : BaseViewModel
 	{
+		private IEventDispatcher _eventDispatcher;
+
 		public ICommand NewProjectCommand => new Command(OnNewProject);
 		public ICommand OpenProjectCommand => new Command(OnOpenProject);
 		public ICommand SaveProjectCommand => new Command(OnSaveProject);
 		public ICommand SaveAllCommand => new Command(OnSaveAllProject);
 		public ICommand ExportCommand => new Command(OnExport);
 
+		public TopToolBarViewModel(IEventDispatcher eventDispatcher)
+		{
+			_eventDispatcher = eventDispatcher;
+		}
+
 		private void OnNewProject()
 		{
-
+			_eventDispatcher.Invoke(new ProjectEvent(ProjectEvent.InstructionType.New));
 		}
 
 		private void OnOpenProject()
